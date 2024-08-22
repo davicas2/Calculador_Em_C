39c6988361b652a803e7ba2abe1ca5427f1a5d43
davicas2 <163083271+davicas2@users.noreply.github.com>
2024-08-22 11:08:03 -0300

    Decimal Para Binario

--- dscpr(main).c
+++ dscpr(main).c
@@ -1,11 +1,50 @@
 #include <stdio.h>
 
+void DecimalParaBinario(int c){
+
+  int binario[8];
+  int i = 0;
+
+  if(c==0){
+    printf("Reposta: 0\n");
+    return;
+  }
+
+  if(c > 1)
+
+  while(c>0){
+    binario[i] = c % 2;
+    c = c / 2;
+    i++;
+  }
+  printf("Repsota:");
+  for (int j = i - 1; j >= 0; j--){
+    printf("%d", binario[j]);
+  }
+  printf("\n\n");
+}
+
+
 int main(void) {
 
-  int a, b, c, d;
+  int a = 0, b, c, d;
+  int respb = 0;
   
-  printf("Qual operação voce deseja fazer\n 1-De base 2 para outra base\n 2- De base 10 para base com sinal com 16 bits(complemento 2)\n 3-Float e Double\n");
-  scanf("%d %d %d", &a, &b, &c);
-  
-  return 0;
-}
+  while(a!=4){
+  printf("Qual operação voce deseja fazer\n1-De base 2 para outra base\n2- De base 10 para base com sinal com 16 bits(complemento 2)\n3-Float e Double\n4-Sair\n");
+  scanf("%d", &a);
+
+  if (a == 1){
+    printf("Qual base voce deseja escolher\n1-Binario(Base 2 )\n2-Octa(Base 8)\n3-Hexa(Base 16)\n4-Codigo BCD\n");
+    scanf("%d", &b);
+    if(b==1){
+      printf("Qual o numero que deseja converter\n");
+      scanf("%d", &c);
+      DecimalParaBinario(c); 
+      }
+    }
+  if(a==4){
+    break;
+  }
+  }
+} 
