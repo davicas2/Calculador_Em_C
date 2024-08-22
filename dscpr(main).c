edd1863ec06f03253041746fd1241ad46cde9e23
davicas2 <163083271+davicas2@users.noreply.github.com>
2024-08-22 11:09:56 -0300

    Decimal Para Octa

--- dscpr(main).c
+++ dscpr(main).c
@@ -24,12 +24,35 @@ void DecimalParaBinario(int c){
   printf("\n\n");
 }
 
+void DecimalParaOcta(int c){
+
+  int octa[16];
+  int i = 0;
+
+  if(c==0){
+    printf("Reposta: 0\n");
+    return;
+  }
+
+  if(c > 1)
+
+  while(c>0){
+    octa[i] = c % 8;
+    c = c / 8;
+    i++;
+  }
+  printf("Repsota:");
+  for (int j = i - 1; j >= 0; j--){
+    printf("%d", octa[j]);
+  }
+  printf("\n\n");
+}
 
 int main(void) {
 
   int a = 0, b, c, d;
   int respb = 0;
-  
+
   while(a!=4){
   printf("Qual operação voce deseja fazer\n1-De base 2 para outra base\n2- De base 10 para base com sinal com 16 bits(complemento 2)\n3-Float e Double\n4-Sair\n");
   scanf("%d", &a);
@@ -42,6 +65,11 @@ int main(void) {
       scanf("%d", &c);
       DecimalParaBinario(c); 
       }
+    if(b==2){
+      printf("Qual o numero deseja converter\n");
+      scanf("%d" , &c);
+      DecimalParaOcta(c);
+    }
     }
   if(a==4){
     break;
