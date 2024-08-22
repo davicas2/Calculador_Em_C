b0cf7b6b03c100872341b7df4ea2cb18d801ab5d
davicas2 <163083271+davicas2@users.noreply.github.com>
2024-08-21 22:54:48 -0300

    Perguntas

---
+++ dscpr(main).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int a, b, c, d;
+  
+  printf("Qual operação voce deseja fazer\n 1-De base 2 para outra base\n 2- De base 10 para base com sinal com 16 bits(complemento 2)\n 3-Float e Double\n");
+  scanf("%d %d %d", &a, &b, &c);
+  
+  return 0;
+}
